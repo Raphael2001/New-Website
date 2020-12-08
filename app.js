d9394e8fcab96b0c018cffec0ea242ab04e0b9ce
Raphael2001 <raphaelaboohi@gmail.com>
2020-12-08 19:09:40 +0200

    new version

--- app.js
+++ app.js
@@ -1,31 +1,39 @@
+AOS.init({
+    duration: 1200,
+  })
+  
 (function ($) {
     "use strict"; // Start of use strict
 
-    // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="/#"]:not([href="/#"])').click(function () {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('').animate({
-                    scrollTop: (target.offset().top - 56)
-                }, 1000, "easeInOutExpo");
-                return false;
-            }
-        }
-    });
+    // // Smooth scrolling using jQuery easing
+    // $('a.js-scroll-trigger[href*="/#"]:not([href="/#"])').click(function () {
+    //     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    //         var target = $(this.hash);
+    //         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    //         if (target.length) {
+    //             $('').animate({
+    //                 scrollTop: (target.offset().top - 56)
+    //             }, 1000, "easeInOutExpo");
+    //             return false;
+    //         }
+    //     }
+    // });
 
-    // Closes responsive menu when a scroll trigger link is clicked
-    $('.js-scroll-trigger').click(function () {
-        $('.navbar-collapse').collapse('hide');
-    });
-
+  
     $(function () {
         $("#nav-placeholder").load("Objects/nav.html");
     });
     $(function () {
         $("#footer-placeholder").load("Objects/footer.html");
     });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+
+    $('.js-scroll-trigger').click(function () {
+        $('.navbar-collapse').collapse('hide');
+    });
+
+   
     // Activate scrollspy to add active class to navbar items on scroll
     $('body').scrollspy({
         target: '#nav',
@@ -33,5 +41,39 @@
     });
 
 })(jQuery); // End of use strict
- 
- 
+
+$(document).ready(function () {
+
+
+    let $btns = $('.portfolio .button-group button');
+
+
+    $btns.click(function (e) {
+
+        $('.portfolio .button-group button').removeClass('active');
+        e.target.classList.add('active');
+
+        let selector = $(e.target).attr('data-filter');
+        $('.portfolio .grid').isotope({
+            filter: selector
+        });
+
+        return false;
+    })
+
+
+
+});
+
+$(document).ready(function () {
+    var scroll_pos = 0;
+    $("#mainNav").scroll(function () {
+        scroll_pos = $(this).scrollTop();
+        if (scroll_pos > 210) {
+            $("#mainNav").css('background-color', '#ffffff');
+        } else {
+            $("#mainNav").css('background-color', 'transparent');
+        }
+        console.log(scroll_pos);
+    });
+});
